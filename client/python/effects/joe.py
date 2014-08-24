8fef7ab1e50a1eaddcd9231fa481c6c6dc1d13d1
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2014-08-24 00:51:49 -0400

    cleanup

--- client/python/effects/joe.py
+++ client/python/effects/joe.py
@@ -10,23 +10,6 @@ twopi = 2 * pi
 
 __all__ = ["barberRingEffect"]
 
-
-class CosmicRay(object):
-    def __init__(self):
-        self.theta = random.random() * pi * 2
-        self.z = 14.5;
-        self.velocity = -(2.8 + random.random() * 1.8)
-        self.rotation = 1.2 * random.random() * (1 if random.random() > 0.5 else -1);
-        self.size = 0.05 + 0.05 * random.random()
-        self.lastUpdate = time.time()
-
-    def tick(self):
-        delta = time.time() - self.lastUpdate
-        self.lastUpdate += delta
-
-        self.z += self.velocity * delta;
-        self.theta = (self.theta + self.rotation * delta) % (2 * pi)
-
 things = dict()
 
 def barberRingEffect(tower, state):
