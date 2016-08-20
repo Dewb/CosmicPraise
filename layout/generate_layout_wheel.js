dd99f9d6e3b301ca2e855a328930712ee9f200e3
Michael Dewberry <michael.dewberry@gmail>
2016-08-20 01:32:12 -0400

    Fix up wheel layout generator

--- layout/generate_layout_wheel.js
+++ layout/generate_layout_wheel.js
@@ -2,201 +2,88 @@
 //
 // usage: node generate_layout.js > cosmicpraise.json
 
-/*
-wheel-right
-wheel-left
-door-right
-door-left
-mid-left-long-low
-mid-left-long-mid
-mid-left-long-high
-mid-left-short-low
-mid-left-short-mid
-mid-left-short-high
-mid-right-long-low
-mid-right-long-mid
-mid-right-long-high
-mid-right-short-low
-mid-right-short-mid
-mid-right-short-high
-*/
+var fs = require('fs');
+var path = require('path');
+var curves = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'wheel_strip_curves.json'), 'utf8'));
+
+function scale(pts, axes) {
+	var newpts = [];
+	for(var i = 0; i < pts.length; i++) {
+		newpts.push([0,0,0]);
+		newpts[i][0] = pts[i][0] * axes[0]; 
+		newpts[i][1] = pts[i][1] * axes[1]; 
+		newpts[i][2] = pts[i][2] * axes[2]; 
+	}
+	return newpts;
+}
+
+var wheel_right_inner = curves["wheel_right_inner0"];
+var wheel_right_outer = curves["wheel_right_outer0"];
+var wheel_left_inner = scale(wheel_right_inner, [-1, 1, 1]);
+var wheel_left_outer = scale(wheel_right_outer, [-1, 1, 1]);
+var back_door_right = curves["back_door_right0"];
+var back_door_left = scale(back_door_right, [-1, 1, 1]);
+var ceiling_center = curves["ceiling_center0"];
+var ceiling_right_high = curves["ceiling_right_high0"];
+var ceiling_right_low = curves["ceiling_right_low0"]
+var ceiling_left_high = scale(ceiling_right_high, [-1, 1, 1]);
+var ceiling_left_low = scale(ceiling_right_low, [-1, 1, 1]);
 
 var lightTypes = [
 	{ 
 		group: "wheel-right", 
 		proto: "opc", address: "10.0.0.32:7890", 
-		ports: [0, 1],
-	  	p0: [25.92, 24.764, 17.292], center: [25.92, 0, 58.089], pixels: 1, 
-	  	arclength: Math.PI * 5/3, radialrepeat: 113, axis: "x", striplength: 120,
-	  	size: 0.1
+		ports: { 0: wheel_right_inner, 1: wheel_right_outer },
 	},
 	{ 
 		group: "wheel-left", 
 		proto: "opc", address: "10.0.0.32:7890", 
-		ports: [2, 3],
-	  	p0: [-25.92, 24.764, 17.292], center: [-25.92, 0, 58.089], pixels: 1, 
-	  	arclength: Math.PI * 5/3, radialrepeat: 113, axis: "x", striplength: 120,
-	  	size: 0.1
+		ports: { 2: wheel_left_inner, 3: wheel_left_outer },
 	},
 	{ 
 		group: "door-right", 
 		proto: "opc", address: "10.0.0.32:7890", 
-		ports: [4],
-	  	p0: [49.40, 54.84, 5.852], center: [49.40, 54.84, 43.631], pixels: 1, scale: [0.3, 1, 1],
-	  	arclength: Math.PI * 2, radialrepeat: 113, axis: "y", striplength: 120,
-	  	size: 0.1
+		ports: { 4: back_door_right },
 	},
 	{ 
 		group: "door-left", 
 		proto: "opc", address: "10.0.0.32:7890", 
-		ports: [5],
-	  	p0: [-49.40, 54.84, 5.852], center: [-49.40, 54.84, 43.631], pixels: 1, scale: [0.3, 1, 1],
-	  	arclength: Math.PI * 2, radialrepeat: 113, axis: "y", striplength: 120,
-	  	size: 0.1
-	},
-	/*
-	{
-		group: "entry-left",
-		proto: "opc", address: "10.0.0.32:7890",
-		ports: [6],
-		p0: [0,0,0], p1: [0,0,0], pixels: 113, arch: 0.75
+		ports: { 5: back_door_left },
 	},
 	{
-		group: "entry-right",
+		group: "ceiling",
 		proto: "opc", address: "10.0.0.32:7890",
-		ports: [7],
-		p0: [0,0,0], p1: [0,0,0], pixels: 113, arch: 0.75
+		ports: { 6: ceiling_right_low, 7: ceiling_right_high, 8: ceiling_center, 9: ceiling_left_high, 10: ceiling_left_low },
 	}
-	*/
 ];
 
-var pixelsPerStrip = 120
+var pixelsPerStrip = 120;
+var pixeldata = [];
 
-var center = [0, 0, 0];
-
-var points = [];
-
-function lerp(a, b, s) {
-	return a + (b - a) * s;
-}
-
-function lerp3(va, vb, s) {
-	return [lerp(va[0], vb[0], s), lerp(va[1], vb[1], s), lerp(va[2], vb[2], s)];
-}
-
-function rotate(v, r, center, axis, scale) {
-	axis = axis || "z";
-	scale = scale || [1, 1, 1];
-	var d = [v[0] - center[0], v[1] - center[1], v[2] - center[2]];
-	var s = Math.sin(r);
-	var c = Math.cos(r);
-
-	var dn = [0, 0, 0];
-	if (axis == "z") {
-		dn = [d[0] * c - d[1] * s, d[0] * s + d[1] * c, d[2]];
-	}
-	else if (axis == "x") {
-		dn = [d[0], d[1] * c - d[2] * s, d[1] * s + d[2] * c];
-	}
-	if (axis == "y") {
-		dn = [d[2] * s + d[0] * c, d[1], d[2] * c - d[0] * s];
-	}
-
-	dn[0] *= scale[0];
-	dn[1] *= scale[1];
-	dn[2] *= scale[2];
-	
-	return [dn[0] + center[0], dn[1] + center[1], dn[2] + center[2]];
+function transformPt(point_in) {
+	var point_out = [0,-50,0];
+	point_out[0] += point_in[0] * 0.25;
+	point_out[1] += point_in[2] * 0.25;
+	point_out[2] += point_in[1] * 0.25;
+	return point_out;
 }
 
 for (var tt = 0; tt < lightTypes.length; tt++) {
-	var type = lightTypes[tt];
-	var rrepeat = type.radialrepeat ? type.radialrepeat : 1;
-	var zrepeat = type.zrepeat ? type.zrepeat : 1;
-	var arclength = type.arclength || Math.PI * 2;
+	var ltype = lightTypes[tt];
 
-	for (var zz = 0; zz < zrepeat; zz++) {
-		for (var ii = 0; ii < rrepeat; ii++) {
-			var r = ii * arclength / rrepeat + ("startangle" in type ? type.startangle : 0);
-			for (var pp = 0; pp < type.pixels; pp++) {
-				var item = {
-					group: type.group,
-					protocol: type.proto
-				};
+	var portIds = Object.keys(ltype.ports);
+	portIds.sort();
 
-				var address = "127.0.0.1:7890";
-				if ("address" in type) {
-					address = type.address;
-				} else if ("addresses" in type) {
-					var addrindex = Math.floor(type.addresses.length * (zz * rrepeat + ii) / (zrepeat * rrepeat));
-					address = type.addresses[addrindex];
-				}
-				item.address = address;
-
-				if (type.proto == "opc") {
-					var port = 0;
-					if ("port" in type) {
-						port = type.port;
-					} else if ("ports" in type) {
-						var portindex = Math.floor(type.ports.length * (zz * rrepeat + ii) / (zrepeat * rrepeat));
-						port = type.ports[portindex];
-					}
-					item.strip = port;
-					pixelIndex = pp;
-					if ("striplength" in type && type.striplength != type.pixels) {
-						pixelIndex = (zz * rrepeat * type.pixels + ii * type.pixels + pp) % type.striplength;
-					}
-					item.index = port * pixelsPerStrip + pixelIndex;
-				} else if (type.proto == "kinet") {
-					if ("address" in type) {
-						item.index = zz * rrepeat + ii;
-					} else if ("addresses" in type) {
-						// do a bunch of unnecessary math because Chooch is playing the Spin Doctors and I can't think straight
-						//var pixelsPerAddress = zrepeat * rrepeat / type.addresses.length;
-						var overallIndex = zz * rrepeat + ii;
-						//var addrindex = Math.floor(type.addresses.length * (zz * rrepeat + ii) / (zrepeat * rrepeat));
-						//item.index = overallIndex - pixelsPerAddress * addrindex;
-                                                item.index = overallIndex
-					}
-				}
-
-				if ("p0" in type) {
-					var p;
-					var p0 = rotate(type.p0, r, type.center || center, type.axis, type.scale);
-					if ("p1" in type) {
-						var p1 = rotate(type.p1, r, type.center || center);
-						p = lerp3(p0, p1, pp / type.pixels);
-					} else {
-						p = p0;
-					}
-					p[2] = p[2] + 0.15 * zz;
-					if ("arch" in type) {
-						p[2] = p[2] + (1 - Math.abs(pp - type.pixels/2) / type.pixels/2) * type.arch;
-					}
-					item.point = p;
-				}
-				if ("point" in type) {
-					item.point = rotate(type.point, r, type.center || center, type.axis, type.scale);
-				}
-				if ("quad" in type) {
-					item.quad = [
-						rotate(type.quad[0], r, type.center || center, type.axis, type.scale),
-						rotate(type.quad[1], r, type.center || center, type.axis, type.scale),
-						rotate(type.quad[2], r, type.center || center, type.axis, type.scale),
-						rotate(type.quad[3], r, type.center || center, type.axis, type.scale),
-					];
-				}
-				if ("size" in type) {
-					item.size = type.size;
-				}
-
-				item.point[0] = item.point[0] / 10;
-				item.point[1] = item.point[1] / 10;
-				item.point[2] = item.point[2] / 10;
-				points.push(item);
-			}
+	for (var portId of Object.keys(ltype.ports)) {
+		for (var ptidx = 0; ptidx < ltype.ports[portId].length; ptidx++) {
+			var data = { strip: portId, point: transformPt(ltype.ports[portId][ptidx]), index: portId * pixelsPerStrip + ptidx };
+			data.group = ltype.group;
+			data.protocol = ltype.proto;
+			data.address = "address" in ltype ? ltype.address : "127.0.0.1:7890";
+			data.size = 2;
+			pixeldata.push(data);	
 		}
 	}
 }
 
-console.log(JSON.stringify(points, null, '\t'));
+console.log(JSON.stringify(pixeldata, null, '\t'));
