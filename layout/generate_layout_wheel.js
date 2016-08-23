1fc7a8b14e0c6e6d018360b0f1531ddf09ff8367
Michael Dewberry <michael.dewberry@gmail>
2016-08-22 22:30:07 -0400

    Adjust strip layout direction, change front door to 2x 70 pixel strips

--- layout/generate_layout_wheel.js
+++ layout/generate_layout_wheel.js
@@ -4,7 +4,8 @@
 
 var fs = require('fs');
 var path = require('path');
-var curves = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'wheel_strip_curves.json'), 'utf8'));
+var curves = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'wheel_strip_curves_113px.json'), 'utf8'));
+var curves70 = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'wheel_strip_curves_70px.json'), 'utf8'));
 
 function scale(pts, axes) {
 	var newpts = [];
@@ -17,18 +18,34 @@ function scale(pts, axes) {
 	return newpts;
 }
 
-var wheel_right_inner = curves["wheel_right_inner0"];
-var wheel_right_outer = curves["wheel_right_outer0"];
-var wheel_left_inner = scale(wheel_right_inner, [-1, 1, 1]);
-var wheel_left_outer = scale(wheel_right_outer, [-1, 1, 1]);
-var back_door_right = curves["back_door_right0"];
-var back_door_left = scale(back_door_right, [-1, 1, 1]);
+function reverse(pts) {
+	var reversed = []
+	for (var i = pts.length - 1; i >= 0; i--) {
+		reversed.push(pts[i]);
+	}
+	return reversed;
+}
+
+// wheel strips start at bottom and meet at top
+var wheel_right_inner = reverse(curves["wheel_right_inner0"]);
+var wheel_right_outer = reverse(curves["wheel_right_outer0"]);
+var wheel_left_inner = reverse(scale(curves["wheel_right_inner0"], [-1, 1, 1]));
+var wheel_left_outer = reverse(scale(curves["wheel_right_outer0"], [-1, 1, 1]));
+
+// back door and two ceiling strips start on the wheel-side ceiling
+var back_door_right = reverse(curves["back_door_right0"]);
+var back_door_left = reverse(scale(curves["back_door_right0"], [-1, 1, 1]));
+var ceiling_left_high = reverse(scale(curves["ceiling_right_high0"], [-1, 1, 1]));
+var ceiling_left_low = reverse(scale(curves["ceiling_right_low0"], [-1, 1, 1]));
+
+// rest of ceiling and front door start on left side other side
 var ceiling_center = curves["ceiling_center0"];
+var ceiling_right_low = curves["ceiling_right_low0"];
 var ceiling_right_high = curves["ceiling_right_high0"];
-var ceiling_right_low = curves["ceiling_right_low0"]
-var ceiling_left_high = scale(ceiling_right_high, [-1, 1, 1]);
-var ceiling_left_low = scale(ceiling_right_low, [-1, 1, 1]);
-var front_door = curves["front_door0"];
+//var front_door = reverse(curves["front_door0"]);
+var front_door_right = curves70["front_door_70px_right0"];
+var front_door_left = scale(curves70["front_door_70px_right0"], [-1, 1, 1]);
+
 
 var lightTypes = [
 	{ 
@@ -42,12 +59,12 @@ var lightTypes = [
 		ports: { 2: wheel_left_inner, 3: wheel_left_outer },
 	},
 	{ 
-		group: "door-right", 
+		group: "back-door-right", 
 		proto: "opc", address: "10.0.0.32:7890", 
 		ports: { 4: back_door_right },
 	},
 	{ 
-		group: "door-left", 
+		group: "back-door-left", 
 		proto: "opc", address: "10.0.0.32:7890", 
 		ports: { 5: back_door_left },
 	},
@@ -59,7 +76,8 @@ var lightTypes = [
 	{ 
 		group: "front-door",
 		proto: "opc", address: "10.0.0.32:7890",
-		ports: { 11: front_door },
+		//ports: { 11: front_door },
+		ports: { 11: front_door_right, 12: front_door_left },
 	},
 ];
 
@@ -67,7 +85,7 @@ var pixelsPerStrip = 120;
 var pixeldata = [];
 
 function transformPt(point_in) {
-	var point_out = [0,-50,0];
+	var point_out = [0,0,0];
 	point_out[0] += point_in[0] * 0.25;
 	point_out[1] += point_in[2] * 0.25;
 	point_out[2] += point_in[1] * 0.25;
@@ -86,7 +104,7 @@ for (var tt = 0; tt < lightTypes.length; tt++) {
 			data.group = ltype.group;
 			data.protocol = ltype.proto;
 			data.address = "address" in ltype ? ltype.address : "127.0.0.1:7890";
-			data.size = 2;
+			data.size = 0.5;
 			pixeldata.push(data);	
 		}
 	}
